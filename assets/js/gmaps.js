1d657b2d52df5578112ead7dc13819e0fb862ec6
michaelmagee <59846293+michaelmagee@users.noreply.github.com>
2020-07-01 11:41:36 -0400

    Added the completed sample files to git

---
+++ assets/js/gmaps.js
@@ -0,0 +1,32 @@
+// Called by  by the google map API  
+// See https://developers.google.com/maps/documentation/javascript/marker-clustering 
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+        // Salem : -70.889143 42.520853
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var locations = [
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 41.084045, lng: -73.874245 },
+        { lat: 42.520853, lng: -70.889143 }
+    ];
+
+    var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+
+    });
+
+    // Add a marker clusterer to manage the markers.
+    var markerCluster = new MarkerClusterer(map, markers,
+        { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
+
